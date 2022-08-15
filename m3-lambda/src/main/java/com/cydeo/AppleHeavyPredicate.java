f5bb179c6be04ec85844727f7a91a02665232d72
Nmerkel <noramerkel0@gmail.com>
2022-08-15 14:24:48 -0400

    Created AppleTest main method to test our project

--- m3-lambda/src/main/java/com/cydeo/AppleHeavyPredicate.java
+++ m3-lambda/src/main/java/com/cydeo/AppleHeavyPredicate.java
@@ -4,6 +4,6 @@ public class AppleHeavyPredicate implements ApplePredicate{
 
     @Override
     public boolean test(Apple apple) {
-        return false;
+        return apple.getWeight()>200;
     }
 }
---
+++ m3-lambda/src/main/java/com/cydeo/AppleTest.java
@@ -0,0 +1,10 @@
+package com.cydeo;
+
+public class AppleTest {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
