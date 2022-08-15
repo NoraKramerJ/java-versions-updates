ce0e4f91ccf030d744657964f9a210598e905e9e
Nmerkel <noramerkel0@gmail.com>
2022-08-14 23:17:32 -0400

    creating switch case with enums

--- m2-enum/src/main/java/com/cydeo/main.java
+++ m2-enum/src/main/java/com/cydeo/main.java
@@ -8,22 +8,34 @@ public class main {
 
         System.out.println("How to access a constant?");
 
-        Currency c=Currency.DIME;// this is already overridden toString enum
+        Currency c=Currency.DIME;
+        // to get one individual enum
+        // this is already overridden toString method with enum
         System.out.println(c);
-        switch(Currency.NICKLE){
-            case DIME:
-                System.out.println(" ten pennies");
-                break;
+        System.out.println(Currency.QUARTER);
+        System.out.println("how to get all constants");
+        Currency[] currencies=Currency.values();
+        // to get all constants, we can put it in an array format,
+        // then iterate it
+
+        for (Currency currency : currencies) {
+            System.out.println(currency);
+        }
+        // we don't use new keyword here because it is enums
+        Currency myCurrency = Currency.QUARTER;
+        switch(myCurrency){
             case PENNY:
-                System.out.println("one penny");
+                System.out.println("It is 1 cent");
                 break;
             case NICKLE:
-                System.out.println(" fifteen penny");
+                System.out.println("It is 5 cent");
+                break;
+            case DIME:
+                System.out.println("It is 10 cent");
                 break;
             case QUARTER:
-                System.out.println("25 cents");
+                System.out.println("It is 25 cent");
                 break;
-
         }
     }
 }
