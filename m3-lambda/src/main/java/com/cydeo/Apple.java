f1d95ebe0d76a4f1200d8700150ed29362962fa5
Nmerkel <noramerkel0@gmail.com>
2022-08-15 14:23:41 -0400

    create apple class and interfaces to get color and sizes of the apples

--- m3-lambda/src/main/java/com/cydeo/Apple.java
+++ m3-lambda/src/main/java/com/cydeo/Apple.java
@@ -1,28 +1,20 @@
 package com.cydeo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter// annotation
+@Setter
+@AllArgsConstructor
+@ToString
+
+
 public class Apple {
 
     private int weight;
     private Color color;// FIXED SO WE CREATE ENUM
 
-    public Apple(int weight, Color color) {
-        this.weight = weight;
-        this.color = color;
-    }
 
-    public int getWeight() {
-        return weight;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    @Override
-    public String toString() {
-        return "Apple{" +
-                "weight=" + weight +
-                ", color=" + color +
-                '}';
-    }
 }
