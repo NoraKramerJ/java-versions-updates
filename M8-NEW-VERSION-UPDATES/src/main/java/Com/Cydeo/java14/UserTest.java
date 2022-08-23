141771dbd2fc93f5e8872ba03822b67efa33b734
Nmerkel <noramerkel0@gmail.com>
2022-08-23 15:11:57 -0400

    Java 14 record

---
+++ M8-NEW-VERSION-UPDATES/src/main/java/Com/Cydeo/java14/UserTest.java
@@ -0,0 +1,11 @@
+package Com.Cydeo.java14;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+
+        User user=new User("mike","2140");
+        System.out.println(user.name());
+        System.out.println(user.toString());
+    }
+}
