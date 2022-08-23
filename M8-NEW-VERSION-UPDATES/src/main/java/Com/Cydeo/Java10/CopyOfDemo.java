bd4cd5c8c7a902fa98f560a9ec6b6b8bc766d14c
Nmerkel <noramerkel0@gmail.com>
2022-08-23 12:51:53 -0400

    Difference between of,copyOf

--- M8-NEW-VERSION-UPDATES/src/main/java/Com/Cydeo/Java10/CopyOfDemo.java
+++ M8-NEW-VERSION-UPDATES/src/main/java/Com/Cydeo/Java10/CopyOfDemo.java
@@ -1,4 +1,16 @@
 package Com.Cydeo.Java10;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CopyOfDemo {
+
+    public static void main(String[] args) {
+
+
+        List<Integer> numberLIst= Arrays.asList(2,4,6,8);
+        List<Integer> unmodifiableLIst2=List.of(2,4,5,6);// CREATING THE LIST WHEN USING
+        List<Integer> unmodifiableLIst=List.copyOf(numberLIst);
+        //this one takes a ready list or collection
+    }
 }
