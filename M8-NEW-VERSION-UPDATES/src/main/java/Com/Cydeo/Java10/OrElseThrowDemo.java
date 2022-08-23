93f6348404eb6931189d5321ec8da4feca5df4f9
Nmerkel <noramerkel0@gmail.com>
2022-08-23 12:46:47 -0400

    Difference between get() and orElseThrow

--- M8-NEW-VERSION-UPDATES/src/main/java/Com/Cydeo/Java10/OrElseThrowDemo.java
+++ M8-NEW-VERSION-UPDATES/src/main/java/Com/Cydeo/Java10/OrElseThrowDemo.java
@@ -15,6 +15,10 @@ public class OrElseThrowDemo {
         }
         System.out.println(str.orElseThrow());
 
-        //get() will throws you NoSuchElementException but, with orElseThrow, you can throw the exception you want.
+        //get() will throw you NoSuchElementException but, with orElseThrow, you can throw the exception you want.
+        //Imagine using .get() method in your application in a lot of places, so if you get NoSuchElementException
+        // you won't be able to know from which .get() method it is coming,
+        // but with orElseThrow() you will be able to throw any exception you want in anywhere in your application,
+        // so that you can know exactly where 3exception is coming from.
     }
 }
